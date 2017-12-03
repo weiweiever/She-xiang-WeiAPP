523b034a86b6e5b1167690e1d45fb05900602cb8
weiweiever <z945429484@outlook.com>
2017-12-03 11:29:49 +0800

    new

---
+++ pages/index/index.js
@@ -0,0 +1,93 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../my/my'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      app.globalData.hasUserInfo = true          
+      app.globalData.userInfo = res.userInfo
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        app.globalData.userInfo = res.userInfo
+        app.globalData.hasUserInfo = true
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          app.globalData.hasUserInfo = true
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    app.globalData.hasUserInfo = true
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  reserve: function(){
+    wx.navigateTo({
+      url: '../reserve/reserve'
+    })
+  },
+  about: function () {
+    wx.navigateTo({
+      url: '../about/about'
+    })
+  },
+  train: function () {
+    wx.navigateTo({
+      url: '../train/train'
+    })
+  },
+  photographers: function () {
+    wx.navigateTo({
+      url: '../photographers/photographers'
+    })
+  },
+  onReady: function(){
+    if (!app.globalData.hasUserInfo) {
+      wx.showModal({
+        title: '提示',
+        content: '有关功能需登陆后使用',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+  }
+})
---
+++ pages/reserve/reserve.js
@@ -0,0 +1,123 @@
+// pages/reserve/reserve.js
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var util = require('../../utils/util.js')
+    var now = new Date
+      var year = now.getFullYear()
+      var month = now.getMonth()+1
+      var day = now.getDate()
+    var tomorrow = new Date(now)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+      var year1 = tomorrow.getFullYear()
+      var month1 = tomorrow.getMonth() + 1
+      var day1 = tomorrow.getDate()
+    var afterTomorrow = new Date(now)
+    afterTomorrow.setDate(afterTomorrow.getDate() + 2)
+      var year2 = afterTomorrow.getFullYear()
+      var month2 = afterTomorrow.getMonth() + 1
+      var day2 = afterTomorrow.getDate()
+    var after3 = new Date(now)
+    after3.setDate(after3.getDate() + 3)
+      var year3 = after3.getFullYear()
+      var month3 = after3.getMonth() + 1
+      var day3 = after3.getDate()
+
+      var date= year + '年' + month + '月' + day + '日'
+      var date1= year1 + '年' + month1 + '月' + day1 + '日'
+      var date2= year2 + '年' + month2 + '月' + day2 + '日'
+      var date3= year3 + '年' + month3 + '月' + day3 + '日'
+      var time1= '9:00-12:30'
+      var time2= '12:30-16:00'
+      var time3= '16:00-18:30'
+      var time4= '18:30-21:00'
+      var work1= '摄影师'
+      var work2= '化妆师'
+      var work3= '修图师'
+    app.globalData.time = now
+    app.globalData.date1 = time1
+    app.globalData.date2 = time2
+    app.globalData.date3 = time3
+    this.setData({
+      date,
+      dateRange:[date1, date2, date3],
+      timeRange:[time1, time2, time3,time4],
+      workRange:[work1,work2,work3]
+    })
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      dateValue: e.detail.value
+    })
+  },
+  bindTimeChange: function (e){
+    this.setData({
+      timeValue:e.detail.value
+    })
+  },
+  bindWorkChange: function (e) {
+    this.setData({
+      workValue: e.detail.value
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
